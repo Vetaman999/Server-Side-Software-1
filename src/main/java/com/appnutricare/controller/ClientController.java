e26c91d47bada6c0821e450e004e8a46343a7ac9
Vetaman999 <hugo.quispe12359@gmail.com>
2021-05-01 18:20:05 -0500

    actualizado

--- src/main/java/com/appnutricare/controller/ClientController.java
+++
@@ -1,9 +0,0 @@
-package com.appnutricare.controller;
-
-public class ClientController {
-    private String name;
-
-    public ClientController(String name) {
-        this.name = name;
-    }
-}
--- src/main/java/com/appnutricare/entities/Client.java
+++
@@ -1,9 +0,0 @@
-package com.appnutricare.entities;
-
-public class Client {
-    public String Name;
-
-    public Client(String name) {
-        Name = name;
-    }
-}
