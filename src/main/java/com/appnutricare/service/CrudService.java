b06fead3270ab155469ff23f172999e6353b5c94
zloop1995 <zloop1995@gmail.com>
2021-05-01 20:19:02 -0500

    Update CrudService.java

--- src/main/java/com/appnutricare/service/CrudService.java
+++ src/main/java/com/appnutricare/service/CrudService.java
@@ -1,13 +1,11 @@
 package com.appnutricare.service;
 
-import javassist.bytecode.Opcode;
-
 import java.util.List;
 import java.util.Optional;
 
-public interface CrudService<T>{
-    T save(T t) throws Exception;   //actualizar o crear
+public interface CrudService<T> {
+    T save(T t) throws Exception;
     void delete(Integer id) throws Exception;
-    List<T> getAll() throws Exception;
+    List<T> getAll() throws  Exception;
     Optional<T> getById(Integer id) throws Exception;
 }
