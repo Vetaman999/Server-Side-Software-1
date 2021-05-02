dd2deb1dcffc49a79600d949537e3f7bb669a2b2
DarkMage17 <55953542+DarkMage17@users.noreply.github.com>
2021-05-01 20:24:00 -0500

    Update Client avoid conflicts

--- src/main/java/com/appnutricare/entities/Client.java
+++ src/main/java/com/appnutricare/entities/Client.java
@@ -1,9 +1,34 @@
 package com.appnutricare.entities;
 
-public class Client {
-    public String Name;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    public Client(String name) {
-        Name = name;
-    }
+import javax.persistence.*;
+import java.io.Serializable;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "client")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "username", nullable = false, length = 16)
+    private String username;
+    @Column(name = "password", nullable = false, length = 60)
+    private String password;
+    @Column(name="firstname", nullable = false, length = 50)
+    private String firstName;
+    @Column(name="lastname", nullable = false, length = 50)
+    private String lastName;
+    @Column(name="email", nullable = false, length = 50)
+    private String email;
+    @Column(name="created_at", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
 }
